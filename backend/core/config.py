66888912d3146c1dafc99803263f27468dd4a9df
FIBA00 <fraolbulti0@gmail.com>
2026-02-04 17:17:14 +0300

    Refactor Settings class: standardize formatting and improve readability of database configuration properties

--- backend/core/config.py
+++ backend/core/config.py
@@ -4,13 +4,16 @@ from pydantic import field_validator
 
 
 class Settings(BaseSettings):
-    DATABASE_HOSTNAME: str  
-    DATABASE_PORT: str  
-    DATABASE_NAME: str  
-    DATABASE_PASSWORD: str  
-    DATABASE_USERNAME: str  
+    DATABASE_HOSTNAME: str
+    DATABASE_PORT: str
+    DATABASE_NAME: str
+    DATABASE_PASSWORD: str
+    DATABASE_USERNAME: str
 
-    
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql://{self.DATABASE_USERNAME}:{self.DATABASE_PASSWORD}@{self.DATABASE_HOSTNAME}:{self.DATABASE_PORT}/{self.DATABASE_NAME}"
+
     class Config:
         # Construct absolute path to .env file in the backend root directory
         env_file = os.path.join(
